62465fbb6995798407d49fe67688eed2b816689b
takashi <42316228+k-tokitoh@users.noreply.github.com>
2020-01-02 16:58:55 +0900

    Updates

---
+++ precache-manifest.fec6baf2df4d67c283d58f6f13ff4770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda05649719ddf08153472f787e65eef",
+    "url": "/matching-game/index.html"
+  },
+  {
+    "revision": "f293c309e916d23e1615",
+    "url": "/matching-game/static/css/main.bd722fac.chunk.css"
+  },
+  {
+    "revision": "8e8c518ae32e9b7d1dd9",
+    "url": "/matching-game/static/js/2.e6c2cd59.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/matching-game/static/js/2.e6c2cd59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f293c309e916d23e1615",
+    "url": "/matching-game/static/js/main.10479466.chunk.js"
+  },
+  {
+    "revision": "a1e2883d390604091180",
+    "url": "/matching-game/static/js/runtime-main.b4c58913.js"
+  }
+]);

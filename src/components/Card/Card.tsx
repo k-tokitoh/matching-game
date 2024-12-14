4c0dc3fab7ff8ba029344d2ab36031fe0b768f62
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-12-14 16:06:12 +0900

    Cardを書き換え

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,31 +2,49 @@ import * as React from "react";
 import logo from "~public/logo.svg";
 import "./Card.css";
 
+type Status = "up" | "down" | "taken";
+
 type Props = {
   readonly card: {
-    readonly status: "turnedUp" | "turnedDown" | "taken";
+    readonly status: Status;
     readonly text: string;
   };
   readonly onClick: () => void;
 };
 
+type Attr = {
+  readonly className: string;
+  readonly clickable: boolean;
+  readonly renderContent: (text: string) => JSX.Element;
+};
+
+const attrs = {
+  up: {
+    className: "up",
+    clickable: true,
+    renderContent: (text: string) => <p>{text}</p>,
+  },
+  down: {
+    className: "down",
+    clickable: true,
+    renderContent: () => <img src={logo} alt="" />,
+  },
+  taken: {
+    className: "taken",
+    clickable: false,
+    renderContent: () => undefined,
+  },
+} as const satisfies { [key in Status]: Attr };
+
 export const Card: React.FC<Props> = ({ card, onClick }) => {
-  switch (card.status) {
-    case "turnedUp":
-      return (
-        <div className="card up" onClick={onClick}>
-          <p>{card.text}</p>
-        </div>
-      );
-    case "turnedDown":
-      return (
-        <div className="card down" onClick={onClick}>
-          <img src={logo} alt="Logo" />
-        </div>
-      );
-    case "taken":
-      return <div className="card taken"></div>;
-    default:
-      return null;
-  }
+  const { className, clickable, renderContent } = attrs[card.status];
+
+  return (
+    <div
+      className={`card ${className}`}
+      onClick={clickable ? onClick : undefined}
+    >
+      {renderContent(card.text)}
+    </div>
+  );
 };
